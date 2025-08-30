4730bfad24fc41eaf74e7fb14a6ba7881741c857
Satoru Morishita <104766195+SatoruMorishita@users.noreply.github.com>
2025-08-30 17:15:38 +0900

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,8 @@ def get_data():
    # return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
 
 #追加
 @app.route('/api/plan')
