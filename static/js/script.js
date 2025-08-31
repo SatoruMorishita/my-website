b167f0128f36cdb04b818855c56830eb00e92593
Satoru Morishita <104766195+SatoruMorishita@users.noreply.github.com>
2025-08-31 15:23:52 +0900

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,7 +1,3 @@
-function openModal() {
-  document.getElementById("dataModal").style.display = "block";
-}
-
 //クローズボタン追加
  document.getElementById('api-close-btn').addEventListener('click', function() {
     window.location.href = 'https://satorumorishita.github.io/my-website/about.html';
