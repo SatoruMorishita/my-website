8ccb5de604a8d6c5cc52d90624539bad81eddba8
Satoru Morishita <104766195+SatoruMorishita@users.noreply.github.com>
2025-08-31 15:28:24 +0900

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,10 @@
 //クローズボタン追加
- document.getElementById('api-close-btn').addEventListener('click', function() {
-    window.location.href = 'https://satorumorishita.github.io/my-website/about.html';
-  });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const closeBtn = document.getElementById('api-close-btn');
+  if (closeBtn) {
+    closeBtn.addEventListener('click', function () {
+      window.location.href = 'https://satorumorishita.github.io/my-website/about.html';
+    });
+  }
+});
