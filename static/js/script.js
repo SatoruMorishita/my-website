ce5dceb5f69d188aa769bfc5eb7f55593b2669ab
Satoru Morishita <104766195+SatoruMorishita@users.noreply.github.com>
2025-08-31 14:55:56 +0900

    Update script.js

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,6 @@ function openModal() {
   document.getElementById("dataModal").style.display = "block";
 }
 
+document.getElementById("close-btn").addEventListener("click", function() {
+    window.location.href = "/about";
+  });
