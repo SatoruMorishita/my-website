95268d17515a2663e0a2c839e2eb5e25e16c0348
Satoru Morishita <104766195+SatoruMorishita@users.noreply.github.com>
2025-09-07 10:36:34 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ import base64
 import matplotlib.font_manager as fm
 
 # 日本語フォントを指定（環境に応じて変えてOK）
-plt.rcParams['font.family'] = 'IPAexGothic'  # または 'Noto Sans CJK JP', 'Yu Gothic', 'MS Gothic' など
+plt.rcParams['font.family'] = 'MS Gothic'  # または 'Noto Sans CJK JP', 'Yu Gothic', 'MS Gothic' など
 
 app = Flask(__name__)
 CORS(app)
