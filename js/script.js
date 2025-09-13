de03721608ac5d3d1cfdd7bd7e8d4c4351788224
Satoru Morishita <104766195+SatoruMorishita@users.noreply.github.com>
2025-09-13 11:35:09 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -143,3 +143,29 @@ document.addEventListener('DOMContentLoaded', () => {
     console.warn("planButton が見つかりません");
   }
 });
+
+//クローズボタン追加
+
+document.addEventListener("DOMContentLoaded", function () {
+  const closeBtn = document.getElementById('api-close-btn');
+  if (closeBtn) {
+    closeBtn.addEventListener('click', function () {
+      window.location.href = 'https://satorumorishita.github.io/my-website/about.html';
+    });
+  }
+});
+
+//toiawase
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  // 入力値の取得
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const subject = document.getElementById("subject").value;
+  const message = document.getElementById("message").value;
+
+  // ここでfetchなどを使ってバックエンドに送信する
+  // 今は仮で完了メッセージを表示
+  document.getElementById("responseMessage").textContent = "お問い合わせありがとうございます！";
+});
